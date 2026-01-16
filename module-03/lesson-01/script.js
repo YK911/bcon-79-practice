73cc2b41e90cb8cc25fd2be991e06893ce958640
Artem Bagno <bgn.rtm@gmail.com>
2026-01-16 15:54:17 +0200

    solution module 2

--- module-03/lesson-01/script.js
+++ module-03/lesson-01/script.js
@@ -23,7 +23,18 @@
 // Task-12
 
 // Task-13
+const b13 = document.querySelector("button.b13");
+// const inp3v1 = document.querySelector("input.i-31");
+// const inp3v2 = document.querySelector("input.i-32");
+const pre13 = document.querySelector("div.panel-body>pre");
+console.log("🚀 ~ pre13:", pre13)
+const out13 = document.querySelector("div.out-13");
+b13.onclick = t13;
+console.log("🚀 ~ b13:", b13)
 
+function t13() {
+console.log("🚀 ~ b13:", b13)
+}
 // Task-14
 
 // Task-15
